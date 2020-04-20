361dd70048b941bded99c6c65c95e79875e8092b
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-20 02:18:45 +0200

    Add PostList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './../styles/App.css';
+import PostList from './PostList';
 
 const App = () => {
     return(
         <div className="ui container app">
-            From App Component
+            <PostList />
         </div>
     );
 }
---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+    render() {
+        return(
+            <div>
+                From Post List component
+            </div>
+        );
+    }
+}
+
+export default PostList;
