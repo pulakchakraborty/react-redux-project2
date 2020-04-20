011e3b4281eadd94e7133e1952999a1d2f4127d8
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-20 14:05:04 +0200

    Wire up action creator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from './../actions';
 
 class PostList extends Component {
+    componentDidMount = () => {
+        this.props.fetchPosts();
+    }
+
     render() {
         return(
             <div>
-                From Post List component
+                From Post List component: {this.props.dummy}
             </div>
         );
     }
 }
 
-export default PostList;
+const MapStateToProps = (State) => {
+    return { dummy: State.dummy };
+}
+
+export default connect(MapStateToProps, { fetchPosts }) (PostList);
