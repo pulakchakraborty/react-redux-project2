984d33bca5b4bb478372a31d868af387e660a991
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-20 15:47:56 +0200

    Use Redux Thunk to wire up async function - Use axios to get posts - async await can't work with normal action creator - use redux thunk as a middleware to wire up the redux store and action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from './../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+    const responseData = await jsonPlaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: responseData });
 };
