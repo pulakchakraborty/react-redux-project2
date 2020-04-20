7c7b9912eed880773cb61c8683b1441b8a13ab41
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-21 00:19:14 +0200

    Displaying the list of posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,11 +8,27 @@ class PostList extends Component {
         this.props.fetchPosts();
     }
 
+    renderList() {
+        return this.props.posts.map(post => {
+            return(
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user"></i>
+                    <div className="content">
+                        <div className="description">
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         console.log(this.props.posts);
         return(
-            <div>
-                From Post List component:
+            <div className="ui relaxed divided list">
+                {this.renderList()}
             </div>
         );
     }
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,7 @@
 export default (state= [], action) => {
     switch (action.type) {
         case 'FETCH_POSTS':
-            return action.payload;
+            return action.payload.data;
         default:
             return state;
     }
