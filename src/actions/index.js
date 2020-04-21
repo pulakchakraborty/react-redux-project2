6b1dc19195964e33cc52f1eaaa9bdfbb9ce83dc5
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-21 21:58:58 +0200

    Creating a new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,8 @@ export const fetchPosts = () => async dispatch => {
     const responseData = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: responseData });
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const responseData = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: responseData });
+};
