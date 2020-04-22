8aa43b6df93e65734d09e2daed51c823b0af71f5
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-22 03:18:09 +0200

    Memoizing Function to be used in action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
 import jsonPlaceholder from './../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
     const responseData = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: responseData });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async(id, dispatch) => {
     const responseData = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: responseData });
-};
+});
