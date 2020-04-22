f654fda15b86f73b07962f90c3e018a545a67f90
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-04-22 02:05:38 +0200

    Wiring up AC, find relavant users by creating new reducer and using mapstatetoprops

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from './../actions';
 
 class UserHeader extends Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.userId);
+    }
+
     render() {
+        const user = this.props.users.find((user) => user.id === this.props.userId);
+
+        if (!user) {
+            return null;
+        }
+
         return(
             <div>
-                From UserHEader Component {this.props.userId}
+                From UserHEader Component {user.name}
             </div>
         );
     }
 };
 
-export default UserHeader;
+const MapStateToProps = (state) => {
+    return {users: state.users};
+}
+
+export default connect(MapStateToProps, { fetchUser }) (UserHeader);
